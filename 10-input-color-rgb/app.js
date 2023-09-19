52d607e4545c1a16937a2058045503333e3d9097
marcodlapaz <marcovalencia.dev@gmail.com>
2023-09-19 15:03:59 -0400

    added 10 input color rgb

---
+++ 10-input-color-rgb/app.js
@@ -0,0 +1,45 @@
+const inputRojo = document.getElementById("rojo");
+const inputVerde = document.getElementById("verde");
+const inputAzul = document.getElementById("azul");
+
+const textoRojo = document.getElementById("texto-rojo");
+const textoVerde = document.getElementById("texto-verde");
+const textoAzul = document.getElementById("texto-azul");
+
+let rojo = inputRojo.value;
+let verde = inputVerde.value;
+let azul = inputAzul.value;
+
+textoRojo.innerText = rojo;
+textoVerde.innerText = verde;
+textoAzul.innerText = azul;
+
+function actualizarColor(rojo, verde, azul) {
+  const colorRGB = `rgb(${rojo}, ${verde}, ${azul})`;
+
+  document.body.style.backgroundColor = colorRGB;
+}
+
+// Para actualizar Rojo (Red)
+inputRojo.addEventListener("change", () => {
+  rojo = inputRojo.value;
+  textoRojo.innerText = rojo;
+
+  actualizarColor(rojo, verde, azul);
+});
+
+// Para actualizar Verde (Green)
+inputVerde.addEventListener("change", () => {
+  verde = inputVerde.value;
+  textoVerde.innerText = verde;
+
+  actualizarColor(rojo, verde, azul);
+});
+
+// Para actualizar Azúl (Blue)
+inputAzul.addEventListener("change", () => {
+  azul = inputAzul.value;
+  textoAzul.innerText = azul;
+
+  actualizarColor(rojo, verde, azul);
+});
