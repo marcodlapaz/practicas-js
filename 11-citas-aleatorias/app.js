d6e99751467eef010963568a26e8e5cc33495d64
marcodlapaz <marcovalencia.dev@gmail.com>
2023-09-21 16:05:08 -0400

    Added 11

---
+++ 11-citas-aleatorias/app.js
@@ -0,0 +1,20 @@
+let botonElem = document.getElementById("boton-cambiar-cita");
+let citaElem = document.getElementById("cita");
+let autorElem = document.getElementById("autor");
+
+function generarEnteroAleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function cambiarCita() {
+  let indiceAleatorio = generarEnteroAleatorio(0, citas.length);
+
+  citaElem.innerText = `"${citas[indiceAleatorio].texto}"`;
+  autorElem.innerText = citas[indiceAleatorio].autor;
+}
+
+let indiceAleatorio = generarEnteroAleatorio(0, citas.length);
+
+cambiarCita();
+
+botonElem.addEventListener("click", cambiarCita)
---
+++ 11-citas-aleatorias/citas.js
@@ -0,0 +1,44 @@
+let citas = [
+  {
+    autor: "Albert Einstein",
+    texto: "La imaginación es más importante que el conocimiento.",
+  },
+  {
+    autor: "Isaac Newton",
+    texto: "Lo que sabemos es una gota de agua; lo que ignoramos es el océano.",
+  },
+  {
+    autor: "Leonardo da Vinci",
+    texto: "Simplicidad es la máxima sofisticación.",
+  },
+  {
+    autor: "Nelson Mandela",
+    texto:
+      "La educación es el arma más poderosa que puedes usar para cambiar el mundo.",
+  },
+  {
+    autor: "Marie Curie",
+    texto: "Nunca veo lo que ha sido hecho, veo lo que queda por hacer.",
+  },
+  {
+    autor: "Steve Jobs",
+    texto: "La única forma de hacer un gran trabajo es amar lo que haces.",
+  },
+  {
+    autor: "Mahatma Gandhi",
+    texto: "Sé el cambio que quieres ver en el mundo.",
+  },
+  {
+    autor: "Martin Luther King Jr.",
+    texto:
+      "La oscuridad no puede expulsar a la oscuridad; sólo la luz puede hacerlo.",
+  },
+  {
+    autor: "William Shakespeare",
+    texto: "Ser o no ser, esa es la cuestión.",
+  },
+  {
+    autor: "Pablo Picasso",
+    texto: "Todo lo que puedes imaginar es real.",
+  },
+];
